ce1ef56ef3cd43c5a73911d548d218765bc8c7a5
azamatcoding <azamat.coding@gmail.com>
2022-10-17 16:14:13 +0500

    Initial commit from Create Next App

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer section">
       <div>Footer</div>
     </footer>
   )
--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 const Subscribe = () => {
   return (
     <section className="subscribe section">
-      <div>Subscribe</div>
+      <div className="subscribe__container container">
+        <h1 className="subscribe__title">Get Started with Holux</h1>
+        <p className="subscribe__description">
+          Subscribe and find super attractive price quotes from us, Find your
+          residence soon
+        </p>
+        <Link href="#">
+          <a className="button subscribe__button">Get Started</a>
+        </Link>
+      </div>
     </section>
   )
 }
